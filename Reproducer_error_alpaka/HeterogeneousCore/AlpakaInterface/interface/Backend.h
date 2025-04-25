46ee696329e8c3ba8726af2867eee275d16e019d
leobeltra <leonardobeltrame00@gmail.com>
2025-04-25 12:24:19 +0200

    Added standalone code for reproducing runtime error

---
+++ Reproducer_error_alpaka/HeterogeneousCore/AlpakaInterface/interface/Backend.h
@@ -0,0 +1,42 @@
+// #include "FWCore/Utilities/interface/Exception.h"
+// #include "HeterogeneousCore/AlpakaInterface/interface/Backend.h"
+
+#include <algorithm>
+#include <array>
+
+namespace cms::alpakatools {
+  // Enumeration whose value EDModules can put in the event
+  enum class Backend : unsigned short { SerialSync = 0, CudaAsync = 1, ROCmAsync = 2, TbbAsync = 3, size };
+
+  Backend toBackend(std::string_view name);
+  std::string_view toString(Backend backend);
+}  // namespace cms::alpakatools
+
+namespace {
+  constexpr const std::array<std::string_view, static_cast<short>(cms::alpakatools::Backend::size)> backendNames = {
+      {"SerialSync", "CudaAsync", "ROCmAsync", "TbbAsync"}};
+}
+
+namespace cms::alpakatools {
+  Backend toBackend(std::string_view name) {
+    auto found = std::find(backendNames.begin(), backendNames.end(), name);
+    if (found == backendNames.end()) {
+      // cms::Exception ex("EnumNotFound");
+      // ex << "Invalid backend name '" << name << "'";
+      // ex.addContext("Calling cms::alpakatools::toBackend()");
+      throw "EnumNotFound";
+    }
+    return static_cast<Backend>(std::distance(backendNames.begin(), found));
+  }
+
+  std::string_view toString(Backend backend) {
+    auto val = static_cast<unsigned short>(backend);
+    if (val >= static_cast<unsigned short>(Backend::size)) {
+      // cms::Exception ex("InvalidEnumValue");
+      // ex << "Invalid backend enum value " << val;
+      // ex.addContext("Calling cms::alpakatools::toString()");
+      throw "InvalidEnumValue";
+    }
+    return backendNames[val];
+  }
+}  // namespace cms::alpakatools
